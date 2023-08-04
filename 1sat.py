7867bdf4b7f8811d1d1fdf243c5f278abd5b0bfa
Rachael Brady <rachael.e.brady@gmail.com>
2023-08-03 23:58:01 -0400

    trouble shooting bytes error

--- 1sat.py
+++ 1sat.py
@@ -6,10 +6,13 @@ my_key = Key(environ["BSV_KEY"])
 hex_of_key = my_key.to_hex()
 
 list_of_pushdata = [
-    OP.OP_DUP, OP.OP_HASH160, bytes.fromhex(hex_of_key), OP.OP_EQUALVERIFY, OP.OP_CHECKSIG,
-    OP.OP_FALSE, OP.OP_IF, bytes.fromhex(0x6f7264), OP.OP_1,
-    "text/plain".encode("utf-8"), OP.OP_0, "Mollys are coming".encode("utf-8"), OP.OP_ENDIF
+    OP.OP_DUP.value, OP.OP_HASH160.value, bytes.fromhex(hex_of_key), OP.OP_EQUALVERIFY.value, OP.OP_CHECKSIG.value,
+    OP.OP_FALSE.value, OP.OP_IF.value, bytes.fromhex("6f7264"), OP.OP_1.value,
+    "text/plain".encode("utf-8"), OP.OP_0.value, "Mollys are coming".encode("utf-8"), OP.OP_ENDIF.value
     ]  # encode string to utf-8 encoded bytes]
+
+for x in list_of_pushdata:
+    print(type(x))
 
 txid = my_key.send(
     outputs = [],
