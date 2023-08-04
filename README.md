c04f9c048555c959924dc3bba3e6cd0eec73cb0c
Rachael Brady <rachael.e.brady@gmail.com>
2023-08-03 22:59:40 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+1sat Mollys
+=================
+
+Welcome to the repository for the Molly Match BSV blockchain generative NFT creator using python. This is a test to create on 1satordinals 
+
+
+==============
+
+1. create virtual environment 
+python3 -m venv .venv
+
+2. Create requiremnts.txt file
+
+3. pip install git+https://github.com/Beryllium-Team/bitsv@add_key_beta
+
+4. We will need a private key to your wallet.  I used the BSV_wallet script.  We need to make a local environment, Run ```export BSV_KEY=```
---
+++ wallet.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from bitsv import Key, PrivateKey
+network = "main"
+my_key_test = PrivateKey(network=network)
+print(my_key_test.address, file=open('bsv_public_address.txt','a')),
+#https://austecon.github.io/bitsv/guide/keys.html#wif
+#Wif is more common for your output of your private key
+print(my_key_test.to_wif(), file=open('bsv_private_key.txt','a'))
+
+#https://austecon.github.io/bitsv/guide/keys.html#hex
+#This is the website to the bitsv libary which creates the wallets and keys. Most don't use HEX but uncomment line 10 if you want hex.
+#print(my_key_test.to_hex())
